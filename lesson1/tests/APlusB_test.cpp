e4f9b923a6e829fea30a45ac8f8f0b01f3af8ee2
dmitrip <dmitriprilepski@gmail.com>
2016-06-01 02:48:08 -0400

    pushing initial empty version

---
+++ lesson1/tests/APlusB_test.cpp
@@ -0,0 +1,14 @@
+/*
+ * APlusB_test.cpp
+ *
+ *  Created on: May 31, 2016
+ *      Author: dmitrip
+ */
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+
