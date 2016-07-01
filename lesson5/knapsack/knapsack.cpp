6d3d69b7461ebd7dfcbbde9c3780adb64dbd00f7
dmitriprilepski@gmail.com <dmitriprilepski@gmail.com>
2016-07-01 02:21:54 -0400

    validated and accepted version

--- lesson5/knapsack/knapsack.cpp
+++ lesson5/knapsack/knapsack.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+long optimal_weight(int W, const vector<int> &w) {
+  // handle corner cases here
+  if (W == 0 || w.size() == 0)
+    return 0; // zero capacity or no weights defined
+
+  vector< vector<long> > matrix(W + 1, vector<long>(w.size() + 1));
+  for(size_t j = 0; j < w.size() + 1; ++j){
+    for(size_t i = 0; i < matrix.size(); ++i){
+      if (i == 0 || j == 0)
+        matrix[i][j] = 0;
+      else{
+        if (i < w[j-1])
+          // not enough capacity for current weight. Use previous weight
+          matrix[i][j] = matrix[i][j-1];
+        else{
+          matrix[i][j] = std::max(matrix[i][j - 1], matrix[std::max(0, (int)(i - w[j - 1]))][j - 1] + w[j - 1]);
+          if (matrix[i][j] > W){
+            // too much weight. Do not add this element
+            matrix[i][j] = std::max(matrix[i][j - 1], matrix[std::max(0, (int)(i - w[j - 1]))][j - 1]);
+          }
+        }
+      }
     }
   }
-  return current_weight;
+
+  return matrix[W][w.size()];
 }
 
 int main() {
